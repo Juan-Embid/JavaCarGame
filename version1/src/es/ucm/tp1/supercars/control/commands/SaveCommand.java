c5708060e9e3bb3d6220f917c69a504d1e8279d7
Juan-Embid <jembid@ucm.es>
2021-12-01 09:55:21 +0100

    Added los nuevos tests y cuatro nuevos commandos

---
+++ version1/src/es/ucm/tp1/supercars/control/commands/SaveCommand.java
@@ -0,0 +1,21 @@
+package es.ucm.tp1.supercars.control.commands;
+
+import es.ucm.tp1.supercars.logic.Game;
+
+public class SaveCommand extends Command{
+	private static final String NAME = "save";
+
+	private static final String DETAILS = "sa[v]e <filename>";
+
+	private static final String SHORTCUT = "v";
+
+	private static final String HELP = "Save the state of the game to a file.";
+	
+	public SaveCommand() {
+		super(NAME, SHORTCUT, DETAILS, HELP);}
+
+	@Override
+	public boolean execute(Game game) {
+		return false;
+	}
+}
